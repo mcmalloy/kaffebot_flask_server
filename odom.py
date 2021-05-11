22d1421560e3f4b1051b7bf8a53db6b036d19f87
Mark Malloy <markmalloy96@yahoo.dk>
2021-05-11 14:04:38 +0200

    Adding odom

---
+++ odom.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+import json
+from nav_msgs.msg import Odometry
+import threading
+
+
+def fetch_odom_data():
+    print("...Initializing ROS Node...")
+    threading.Thread(target=lambda: rospy.init_node('battery_pub', disable_signals=True)).start()
+    odom_data = rospy.wait_for_message("odom", Odometry, timeout=None)
+    print("Odom data: ", odom_data)
+
+
+if __name__ == '__main__':
+    try:
+        fetch_odom_data()
+    except rospy.ROSInterruptException:
+        pass
