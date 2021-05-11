4aeecc1e08b66f0f267587d0d83eca7e31df35f7
Mark Malloy <markmalloy96@yahoo.dk>
2021-05-11 11:03:53 +0200

    test print of values

--- battery.py
+++ battery.py
@@ -40,14 +40,16 @@ def battery_charge_listener():
 
 def listener():
     rospy.init_node('battery_pub')
-    voltage = rospy.wait_for_message("battery/voltage", Float32, timeout=None)
-    current = rospy.wait_for_message("battery/current", Float32, timeout=None)
-    charge = rospy.wait_for_message("battery/charge", Float32, timeout=None)
-    temp = rospy.wait_for_message("battery/temperature", Int16, timeout=None)
-    print("Voltage: %s [V]", voltage)
-    print("Current: %s [A]", current)
-    print("Charge: %s [Ah]", charge)
-    print("Temperature: %s [C]", temp)
+    rospy.Rate(1)
+    while not rospy.is_shutdown():
+        voltage = rospy.wait_for_message("battery/voltage", Float32, timeout=None)
+        current = rospy.wait_for_message("battery/current", Float32, timeout=None)
+        charge = rospy.wait_for_message("battery/charge", Float32, timeout=None)
+        temp = rospy.wait_for_message("battery/temperature", Int16, timeout=None)
+        print("Voltage: [V]", voltage)
+        print("Current: [A]", current)
+        print("Charge: [Ah]", charge)
+        print("Temperature: [C]", temp)
 
 
 if __name__ == '__main__':
